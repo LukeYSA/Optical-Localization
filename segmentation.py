de6f0ccfd4f9f0159d3aaa74fc27ffed2e00f117
Luke Yin <lukeysa@uw.edu>
2022-02-11 00:02:53 -0800

    Changed approach

--- segmentation.py
+++ segmentation.py
@@ -5,60 +5,19 @@ import os
 path = r'/Users/lukeyin/Desktop/School/Mirror/Research/Optical-Localization/input_img'
 output_dir = r'/Users/lukeyin/Desktop/School/Mirror/Research/Optical-Localization/output_img'
 
-img=cv2.imread(path + '/photo.png')
-img_hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+img=cv2.imread(path + '/photo2.png')
+img = cv2.GaussianBlur(img, (7, 7), 0)
+# img = cv2.blur(img, (7, 7))
 
-# lower red mask (0-10)
-lower_red = np.array([0,50,50])
-upper_red = np.array([10,255,255])
-mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
+B, G, R = cv2.split(img)
+ret, mask = cv2.threshold(G, 66, 255, cv2.THRESH_BINARY)
+# mask = cv2.adaptiveThreshold(G, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
-# upper red mask (170-180)
-lower_red = np.array([170,50,50])
-upper_red = np.array([180,255,255])
-mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
-
-# join my red masks
-red_mask = mask0+mask1
-red_mask = cv2.bitwise_not(red_mask)
-
-# green mask
-lower_green = np.array([36,25,25])
-upper_green = np.array([86,255,255])
-green_mask = cv2.inRange(img_hsv, lower_green, upper_green)
-green_mask = cv2.bitwise_not(green_mask)
-
-# white mask
-lower_white = np.array([0,0,200])
-upper_white = np.array([145,60,255])
-white_mask = cv2.inRange(img_hsv, lower_white, upper_white)
-white_mask = cv2.bitwise_not(white_mask)
-
-# black mask
-lower_black = np.array([0,0,0])
-upper_black = np.array([179,50,100])
-black_mask = cv2.inRange(img_hsv, lower_black, upper_black)
-black_mask = cv2.bitwise_not(black_mask)
+edges = cv2.Canny(mask, 100, 200)
 
 os.chdir(output_dir)
-cv2.imwrite('red_mask.png', red_mask)
-cv2.imwrite('green_mask.png', green_mask)
-cv2.imwrite('white_mask.png', white_mask)
-cv2.imwrite('black_mask.png', black_mask)
-
-# Find contours
-green_mask = cv2.bitwise_not(green_mask)
-im2, contours, hierarchy = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-i=5
-for contour in contours:
-    # We don't want too many contours
-    if i > 8:
-        break
-
-    # Bound the contour in a rectange
-    [x, y, w, h] = cv2.boundingRect(contour)
-    print(str(x) + ", " + str(y) + ", " + str(w) + ", " + str(h))
-
-    # Resize the image to only contain the contour
-    cv2.imwrite(str(i)+'.png', img[y:y+h,x:x+w])
-    i=i+1
+cv2.imwrite("blue.png", B)
+cv2.imwrite("green.png", G)
+cv2.imwrite("mask.png", mask)
+cv2.imwrite("edges.png", edges)
+cv2.imwrite("red.png", R)
