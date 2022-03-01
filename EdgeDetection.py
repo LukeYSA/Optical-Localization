6b230208ccf288b3790e81e9c3d6ff49b9d3df81
LukeYSA <lukeysa@icloud.com>
2022-03-01 11:48:23 -0800

    Added feature to detect intersections

--- EdgeDetection.py
+++ EdgeDetection.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import os
+from collections import defaultdict
 
 path = r'/Users/lukeyin/Desktop/Research/Optical-Localization/input_img'
 output_dir = r'/Users/lukeyin/Desktop/Research/Optical-Localization/output_img'
@@ -208,6 +209,61 @@ def detect_cross(img):
             print(str(x) + ", " + str(y))
             print(" ")
 
+def segment_lines(lines, k=2, **kwargs):
+    # Set parameters
+    default_criteria_type = cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER
+    criteria = kwargs.get('criteria', (default_criteria_type, 10, 1.0))
+    flags = kwargs.get('flag', cv2.KMEANS_RANDOM_CENTERS)
+    attempts = kwargs.get('attempts', 10)
+
+    # Get the angles in radians
+    angles = np.array([line[0][1] for line in lines])
+    # Get unit circle coordinates of the angle
+    pts = np.array([[np.cos(2 * angle), np.sin(2 * angle)] for angle in angles], dtype=np.float32)
+
+    # Group the points based on where they are on the unit circle
+    labels, centers = cv2.kmeans(pts, k, None, criteria, attempts, flags)[1:]
+    labels = labels.reshape(-1)
+
+    # Group the lines based on the kmeans grouping
+    segmented = defaultdict(list)
+    for i, line in enumerate(lines):
+        segmented[labels[i]].append(line)
+    segmented = list(segmented.values())
+    return segmented
+
+def intersection(line1, line2):
+    # Find intersection point of two lines from rho and theta
+    # Solve:
+    # x * cos(theta1) + y * sin(theta1) = r1
+    # x * cos(theta2) + y * sin(theta2) = r1
+    rho1, theta1 = line1[0]
+    rho2, theta2 = line2[0]
+    A = np.array([
+        [np.cos(theta1), np.sin(theta1)],
+        [np.cos(theta2), np.sin(theta2)]
+    ])
+    b = np.array([[rho1], [rho2]])
+    x0, y0 = np.linalg.solve(A, b)
+    return [[x0], [y0]]
+
+def segmented_intersection(lines):
+    intersections = []
+    # Compare lines in each group
+    for i, group in enumerate(lines[:-1]):
+        for next_group in lines[i+1:]:
+            for line1 in group:
+                for line2 in next_group:
+                    intersections.append(intersection(line1, line2))
+    return intersections
+
+def plot_points(img, points):
+    copy = img.copy()
+    for point in points:
+        # print(str(point[0][0]) + ", " + str(point[1][0]))
+        copy = cv2.circle(copy, (point[0][0], point[1][0]), radius=5, color=(255, 0, 0), thickness=-1)
+    return copy
+
 if __name__ == '__main__':
     img = cv2.imread(path + '/chess.png')
 
@@ -217,9 +273,17 @@ if __name__ == '__main__':
 
     res = drawFull(img)
         
-    detect_cross(img)
+    # detect_cross(img)
+    rho, theta, thresh = 2, np.pi/180, 400
+    lines = cv2.HoughLines(res, rho, theta, thresh)
+    segmented = segment_lines(lines)
+    intersections = segmented_intersection(segmented)
+
+    # print(str(intersections))
+    points = plot_points(res, intersections)
 
     os.chdir(output_dir)
     # cv2.imwrite("hedges.png", horizontal_edges)
     # cv2.imwrite("vedges.png", vertical_edges)
     cv2.imwrite("res.png", res)
+    cv2.imwrite("points.png", points)
